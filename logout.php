4c0c90e995aaefc82246bfbd402a353d7017f8f9
sneHaJ20 <109842070+sneHaJ20@users.noreply.github.com>
2022-10-06 20:27:17 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php
+    require_once('setup.php');
+    //print_r($_SESSION['token']);
+    // $google->revokeToken($_SESSION['token']);
+
+    // session_destroy();
+    // header('Location:index.php');
+?>
